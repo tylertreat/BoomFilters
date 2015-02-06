5ce78eed5323e057942bd57a35f8330229562b2b
Tyler Treat <tyler.treat@webfilings.com>
2015-02-06 14:22:29 -0600

    Implement classic Bloom filter

---
+++ boom.go
@@ -0,0 +1,41 @@
+/*
+Package boom implements probabilistic data structures for processing
+continuous, unbounded data streams. This includes Stable, Scalable, and Inverse
+Bloom filters as well as traditional Bloom filters, which are actually a
+special case of Stable filters.
+
+Classic Bloom filters generally require a priori knowledge of the data set in
+order to allocate an appropriately sized bit array. This works well for offline
+processing, but online processing typically involves unbounded data streams.
+With enough data, a traditional Bloom filter "fills up", after which it has a
+false-positive probability of 1.
+
+Boom Filters are useful for cases where the size of the data set isn't known
+ahead of time. For example, a Stable Bloom Filter can be used to deduplicate
+events from an unbounded event stream with a specified upper bound on false
+positives and minimal false negatives. Alternatively, an Inverse Bloom Filter
+is ideal for deduplicating a stream where duplicate events are relatively close
+together. This results in no false positives and, depending on how close
+together duplicates are, a small probability of false negatives. Scalable Bloom
+Filters place a tight upper bound on false positives while avoiding false
+negatives but require allocating memory proportional to the size of the data
+set.
+*/
+package boom
+
+import "math"
+
+const fillRatio = 0.5
+
+// OptimalM calculates the optimal Bloom filter size, m, based on the number of
+// items and the desired rate of false positives.
+func OptimalM(n uint, fpRate float64) uint {
+	return uint(math.Ceil(float64(n) / ((math.Log(fillRatio) *
+		math.Log(1-fillRatio)) / math.Abs(math.Log(fpRate)))))
+}
+
+// OptimalK calculates the optimal number of hash functions to use for a Bloom
+// filter based on the desired rate of false positives.
+func OptimalK(fpRate float64) uint {
+	return uint(math.Ceil(math.Log2(1 / fpRate)))
+}
---
+++ classic.go
@@ -0,0 +1,108 @@
+package boom
+
+import (
+	"encoding/binary"
+	"hash"
+	"hash/fnv"
+
+	"github.com/willf/bitset"
+)
+
+// BloomFilter implements a classic Bloom filter. A Bloom filter has a non-zero
+// probability of false positives and a zero probability of false negatives.
+type BloomFilter struct {
+	array *bitset.BitSet // filter data
+	hash  hash.Hash      // hash function (kernel for all k functions)
+	m     uint           // filter size
+	k     uint           // number of hash functions
+	p     float64        // fill ratio
+	n     uint           // number of items to store
+}
+
+// NewBloomFilter creates a new Bloom filter optimized to store n items with a
+// specified target false-positive rate.
+func NewBloomFilter(n uint, fpRate float64) *BloomFilter {
+	m := OptimalM(n, fpRate)
+	return &BloomFilter{
+		array: bitset.New(m),
+		hash:  fnv.New64(),
+		m:     m,
+		k:     OptimalK(fpRate),
+		p:     fillRatio,
+		n:     n,
+	}
+}
+
+// Capacity returns the Bloom filter capacity, m.
+func (b *BloomFilter) Capacity() uint {
+	return b.m
+}
+
+// K returns the number of hash functions.
+func (b *BloomFilter) K() uint {
+	return b.k
+}
+
+// Test will test for membership of the data and returns true if it is a
+// member, false if not. This is a probabilistic test, meaning there is a
+// non-zero probability of false positives but a zero probability of false
+// negatives.
+func (b *BloomFilter) Test(data []byte) bool {
+	lower, upper := b.hashKernel(data)
+
+	// If any of the K bits are not set, then it's not a member.
+	for i := uint(0); i < b.k; i++ {
+		if !b.array.Test((uint(lower) + uint(upper)*i) % b.m) {
+			return false
+		}
+	}
+
+	return true
+}
+
+// Add will add the data to the Bloom filter. It returns the filter to allow
+// for chaining.
+func (b *BloomFilter) Add(data []byte) *BloomFilter {
+	lower, upper := b.hashKernel(data)
+
+	// Set the K bits.
+	for i := uint(0); i < b.k; i++ {
+		b.array.Set((uint(lower) + uint(upper)*i) % b.m)
+	}
+
+	return b
+}
+
+// TestAndSet is equivalent to calling Test followed by Add. It returns true if
+// the data is a member, false if not.
+func (b *BloomFilter) TestAndAdd(data []byte) bool {
+	lower, upper := b.hashKernel(data)
+	member := true
+
+	// If any of the K bits are not set, then it's not a member.
+	for i := uint(0); i < b.k; i++ {
+		idx := (uint(lower) + uint(upper)*i) % b.m
+		if !b.array.Test(idx) {
+			member = false
+		}
+		b.array.Set(idx)
+	}
+
+	return member
+}
+
+// Reset restores the Bloom filter to its original state. It returns the filter
+// to allow for chaining.
+func (b *BloomFilter) Reset() *BloomFilter {
+	b.array.ClearAll()
+	return b
+}
+
+// hashKernel returns the upper and lower base hash values from which the k
+// hashes are derived.
+func (b *BloomFilter) hashKernel(data []byte) (uint32, uint32) {
+	b.hash.Write(data)
+	sum := b.hash.Sum(nil)
+	b.hash.Reset()
+	return binary.BigEndian.Uint32(sum[4:8]), binary.BigEndian.Uint32(sum[0:4])
+}
