ea4778f95ff57175c25b12b790efa8a194187859
Tyler Treat <tyler.treat@webfilings.com>
2015-02-06 15:34:27 -0600

    Implement partitioned Bloom filter

---
+++ partitioned.go
@@ -0,0 +1,130 @@
+package boom
+
+import (
+	"encoding/binary"
+	"hash"
+	"hash/fnv"
+	"math"
+
+	"github.com/willf/bitset"
+)
+
+// PartitionedBloomFilter implements a variation of a classic Bloom filter as
+// described by Almeida, Baquero, Preguica, and Hutchison in Scalable Bloom
+// Filters:
+//
+// http://gsd.di.uminho.pt/members/cbm/ps/dbloom.pdf
+//
+// This filter works by partitioning the M-sized bit array into k slices of
+// size m = M/k bits. Each hash function produces an index over m for its
+// respective slice. Thus, each element is described by exactly k bits, meaning
+// the distribution of false positives is uniform across all elements.
+type PartitionedBloomFilter struct {
+	partitions []*bitset.BitSet // partitioned filter data
+	hash       hash.Hash        // hash function (kernel for all k functions)
+	m          uint             // filter size (divided into k partitions)
+	k          uint             // number of hash functions (and partitions)
+	p          float64          // partition fill ratio
+	s          uint             // partition size (m / k)
+}
+
+// NewPartitionedBloomFilter creates a new partitioned Bloom filter optimized
+// to store n items with a specified target false-positive rate.
+func NewPartitionedBloomFilter(n uint, fpRate float64) *PartitionedBloomFilter {
+	var (
+		m          = OptimalM(n, fpRate)
+		k          = OptimalK(fpRate)
+		partitions = make([]*bitset.BitSet, k)
+		s          = uint(math.Ceil(float64(m) / float64(k)))
+	)
+
+	for i := uint(0); i < k; i++ {
+		partitions[i] = bitset.New(s)
+	}
+
+	return &PartitionedBloomFilter{
+		partitions: partitions,
+		hash:       fnv.New64(),
+		m:          m,
+		k:          k,
+		p:          fillRatio,
+		s:          s,
+	}
+}
+
+// Capacity returns the Bloom filter capacity, m.
+func (p *PartitionedBloomFilter) Capacity() uint {
+	return p.m
+}
+
+// K returns the number of hash functions.
+func (p *PartitionedBloomFilter) K() uint {
+	return p.k
+}
+
+// Test will test for membership of the data and returns true if it is a
+// member, false if not. This is a probabilistic test, meaning there is a
+// non-zero probability of false positives but a zero probability of false
+// negatives. Due to the way the filter is partitioned, the probability of
+// false positives is uniformly distributed across all elements.
+func (p *PartitionedBloomFilter) Test(data []byte) bool {
+	lower, upper := p.hashKernel(data)
+
+	// If any of the K partition bits are not set, then it's not a member.
+	for i := uint(0); i < p.k; i++ {
+		if !p.partitions[i].Test((uint(lower) + uint(upper)*i) % p.s) {
+			return false
+		}
+	}
+
+	return true
+}
+
+// Add will add the data to the Bloom filter. It returns the filter to allow
+// for chaining.
+func (p *PartitionedBloomFilter) Add(data []byte) *PartitionedBloomFilter {
+	lower, upper := p.hashKernel(data)
+
+	// Set the K partition bits.
+	for i := uint(0); i < p.k; i++ {
+		p.partitions[i].Set((uint(lower) + uint(upper)*i) % p.s)
+	}
+
+	return p
+}
+
+// TestAndAdd is equivalent to calling Test followed by Add. It returns true if
+// the data is a member, false if not.
+func (p *PartitionedBloomFilter) TestAndAdd(data []byte) bool {
+	lower, upper := p.hashKernel(data)
+	member := true
+
+	// If any of the K partition bits are not set, then it's not a member.
+	for i := uint(0); i < p.k; i++ {
+		idx := (uint(lower) + uint(upper)*i) % p.s
+		if !p.partitions[i].Test(idx) {
+			member = false
+		}
+		p.partitions[i].Set(idx)
+	}
+
+	return member
+}
+
+// Reset restores the Bloom filter to its original state. It returns the filter
+// to allow for chaining.
+func (p *PartitionedBloomFilter) Reset() *PartitionedBloomFilter {
+	for _, partition := range p.partitions {
+		partition.ClearAll()
+	}
+	return p
+}
+
+// hashKernel returns the upper and lower base hash values from which the k
+// hashes are derived.
+func (p *PartitionedBloomFilter) hashKernel(data []byte) (uint32, uint32) {
+	p.hash.Write(data)
+	sum := p.hash.Sum(nil)
+	p.hash.Reset()
+	return binary.BigEndian.Uint32(sum[4:8]), binary.BigEndian.Uint32(sum[0:4])
+}
