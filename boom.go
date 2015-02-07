85e1b458cef76b50138f25b8d477db9fa26ae670
Tyler Treat <ttreat31@gmail.com>
2015-02-07 00:55:04 -0600

    Update package docstring

--- boom.go
+++ boom.go
@@ -1,25 +1,25 @@
 /*
-Package boom implements probabilistic data structures for processing
-continuous, unbounded data streams. This includes Stable, Scalable, and Inverse
-Bloom filters as well as traditional Bloom filters, which are actually a
-special case of Stable filters.
-
-Classic Bloom filters generally require a priori knowledge of the data set in
-order to allocate an appropriately sized bit array. This works well for offline
-processing, but online processing typically involves unbounded data streams.
-With enough data, a traditional Bloom filter "fills up", after which it has a
-false-positive probability of 1.
-
-Boom Filters are useful for cases where the size of the data set isn't known
-ahead of time. For example, a Stable Bloom Filter can be used to deduplicate
-events from an unbounded event stream with a specified upper bound on false
-positives and minimal false negatives. Alternatively, an Inverse Bloom Filter
-is ideal for deduplicating a stream where duplicate events are relatively close
-together. This results in no false positives and, depending on how close
-together duplicates are, a small probability of false negatives. Scalable Bloom
-Filters place a tight upper bound on false positives while avoiding false
-negatives but require allocating memory proportional to the size of the data
-set.
+// Package boom implements probabilistic data structures for processing
+// continuous, unbounded data streams. This includes Stable Bloom Filters,
+// Scalable Bloom Filters, Inverse Bloom Filters, and several variants of
+// traditional Bloom filters.
+//
+// Classic Bloom filters generally require a priori knowledge of the data set
+// in order to allocate an appropriately sized bit array. This works well for
+// offline processing, but online processing typically involves unbounded data
+// streams. With enough data, a traditional Bloom filter "fills up", after
+// which it has a false-positive probability of 1.
+//
+// Boom Filters are useful for situations where the size of the data set isn't
+// known ahead of time. For example, a Stable Bloom Filter can be used to
+// deduplicate events from an unbounded event stream with a specified upper
+// bound on false positives and minimal false negatives. Alternatively, an
+// Inverse Bloom Filter is ideal for deduplicating a stream where duplicate
+// events are relatively close together. This results in no false positives
+// and, depending on how close together duplicates are, a small probability of
+// false negatives. Scalable Bloom Filters place a tight upper bound on false
+// positives while avoiding false negatives but require allocating memory
+// proportional to the size of the data set.
 */
 package boom
 
