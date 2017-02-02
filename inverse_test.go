dee13ba772e856c8957cca3995b9d9bb52f5bcdc
sam-lowenkamp <sam@slowenkamp-mbp.local>
2017-02-02 17:39:41 -0500

    Add method ImportElementsFrom on inverse bloom filter
    ReadFrom and GobDecode methods allowed a bloom filter to be
    exported and then recreated as part of a new array. The new
    ImportElementsFrom method uses the filter.Add method to add
    the elements written out by one filter to the array of another
    filter.
    
    This allows for use cases such as converting from one
    filter size to another. For example, rolling a size 1,000
    filter into a size 1,000,000 filter.

--- inverse_test.go
+++ inverse_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/gob"
 	"github.com/d4l3k/messagediff"
+	"os"
 	"strconv"
 	"testing"
 )
@@ -70,6 +71,56 @@ func TestInverseTestAndAdd(t *testing.T) {
 	}
 }
 
+// Ensures an InverseBloomFilter can read and write successfully
+func TestInverseBloomFilter_ReadFrom(t *testing.T) {
+	d, err := os.Create("TestInverseBloomFilter_ReadFrom.dat")
+
+	// Write a filter
+	f := NewInverseBloomFilter(10000)
+
+	for i := 0; i < 1000; i++ {
+		f.Add([]byte(strconv.Itoa(i)))
+	}
+
+	if _, err := f.WriteTo(d); err != nil {
+		t.Error(err)
+	}
+	d.Close()
+
+	// Read the filter into a new one
+	f2 := NewInverseBloomFilter(10000)
+	d, err = os.Open("TestInverseBloomFilter_ReadFrom.dat")
+	read, err := f2.ReadFrom(d)
+	if err != nil {
+		t.Error(err)
+	}
+	d.Close()
+
+	if read != 12814 {
+		t.Errorf("Expected to read 12814 bytes, read %v", read)
+	}
+
+	if f.capacity != f2.capacity {
+		t.Error("Different capacities")
+	}
+
+	if len(f.array) != len(f2.array) {
+		t.Error("Different data")
+	}
+
+	if diff, equal := messagediff.PrettyDiff(f.array, f2.array); !equal {
+		t.Errorf("BloomFilter WriteTo and ReadFrom = %+v; not %+v\n%s", f2, f, diff)
+	}
+
+	for i := 0; i < 100000; i++ {
+		if f.Test([]byte(strconv.Itoa(i))) != f2.Test([]byte(strconv.Itoa(i))) {
+			t.Errorf("Expected both filters to Test the same for %d", i)
+		}
+	}
+
+	os.Remove("TestInverseBloomFilter_ReadFrom.dat")
+}
+
 // Tests that an InverseBloomFilter can be encoded and decoded properly without error
 func TestInverseBloomFilter_Encode(t *testing.T) {
 	f := NewInverseBloomFilter(10000)
@@ -89,11 +140,11 @@ func TestInverseBloomFilter_Encode(t *testing.T) {
 	}
 
 	if f.capacity != f2.capacity {
-		t.Errorf("Different capacities")
+		t.Errorf("Expected capacity %v is different from actual capacity %v", f.capacity, f2.capacity)
 	}
 
 	if len(f.array) != len(f2.array) {
-		t.Errorf("Different data")
+		t.Error("Different data between filter 1 and filter 2.")
 	}
 
 	if diff, equal := messagediff.PrettyDiff(f.array, f2.array); !equal {
@@ -102,9 +153,53 @@ func TestInverseBloomFilter_Encode(t *testing.T) {
 
 	for i := 0; i < 100000; i++ {
 		if f.Test([]byte(strconv.Itoa(i))) != f2.Test([]byte(strconv.Itoa(i))) {
-			t.Errorf("Expected both filters to Test the same for %i", i)
+			t.Errorf("Expected both filters to test the same for %d", i)
 		}
 	}
+}
+
+func TestInverseBloomFilter_ImportElementsFrom(t *testing.T) {
+	// Write out a bloom filter of size 3
+	f1 := NewInverseBloomFilter(3)
+	for _, b := range [][]byte{[]byte(`a`), []byte(`b`), []byte(`c`)} {
+		f1.Add(b)
+	}
+
+	d, err := os.Create("TestInverseBloomFilter_ImportElementsFrom.dat")
+	if err != nil {
+		t.Errorf("Failed to create test file: %v", err)
+	}
+
+	f1.WriteTo(d)
+	d.Close()
+
+	// Read the data into a new filter of size 10
+	f2 := NewInverseBloomFilter(5)
+	d, err = os.Open("TestInverseBloomFilter_ImportElementsFrom.dat")
+	if err != nil {
+		t.Errorf("Failed to open test file: %v", err)
+	}
+
+	f2.ImportElementsFrom(d)
+
+	if f2.TestAndAdd([]byte(`a`)) != true {
+		t.Error("f2 should have 'a' but returned false")
+	}
+
+	if f2.TestAndAdd([]byte(`b`)) != true {
+		t.Error("f2 should have 'b' but returned false")
+	}
+
+	if f2.TestAndAdd([]byte(`c`)) != true {
+		t.Error("f2 should have 'c' but returned false")
+	}
+
+	// Assert that the new filter is still of the new size
+	if len(f2.array) != 5 {
+		t.Errorf("Expected len of f2.array to be 5, instead found %v", len(f2.array))
+	}
+
+	os.Remove("TestInverseBloomFilter_ImportElementsFrom.dat")
 }
 
 func BenchmarkInverseAdd(b *testing.B) {
